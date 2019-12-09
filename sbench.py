13910046214cd75bdcd7cc1f5bb9ed3d5a169320
Jonathon Anderson <jonathon.anderson@colorado.edu>
2019-12-08 21:11:18 -0700

    sbench is capable of running jobs

--- sbench.py
+++ sbench.py
@@ -7,30 +7,64 @@ import re
 import subprocess
 import hostlist
 import logging
+import time
 
 
 NODE_NAME_P = re.compile(r'^NodeName=([^ ]+)')
+STATE_P = re.compile(r'State=([^ ]+)')
 NODES_P = re.compile(r' Nodes=([^ \n]+)')
+
+POLLING_INTERVAL=2
 
 
 def main ():
     logging.basicConfig(level=logging.DEBUG)
     parser = argparse.ArgumentParser()
     parser.add_argument('--partition')
+    parser.add_argument('--ntasks')
+    parser.add_argument('--state', action='append')
+    parser.add_argument('--account')
+    parser.add_argument('--chdir')
+    parser.add_argument('--time')
+    parser.add_argument('executable')
+    parser.add_argument('executable_arguments', nargs='*')
     args = parser.parse_args()
 
-    nodes = set(get_all_nodes())
-    logging.debug('all nodes: {0}'.format(len(nodes)))
+    nodes = set(get_all_nodes(states=args.state))
     if args.partition:
         nodes = nodes & set(get_partition_nodes(args.partition))
-        logging.debug('partition nodes: {0}'.format(len(nodes)))
+    nodes = list(sorted(nodes))
+    logging.debug('nodes: {0}'.format(len(nodes)))
+
+    jobs = [srun(args.executable, args.executable_arguments,
+                 partition=args.partition, nodelist=node,
+                 ntasks=args.ntasks, account=args.account,
+                 chdir=args.chdir, time=args.time) for node in nodes]
+
+    completed_jobs = set()
+    new_completed_jobs = set()
+    while completed_jobs != set(jobs):
+        for job in jobs:
+            job.poll()
+        for node, job in zip(nodes, jobs):
+            if job.returncode is not None and job not in completed_jobs:
+                new_completed_jobs.add(job)
+                print(job.returncode, node, job.stdout.read().strip())
+        if new_completed_jobs:
+            logging.debug('completed: {0}/{1}'.format(len(completed_jobs), len(jobs)))
+            completed_jobs |= new_completed_jobs
+            new_completed_jobs = set()
+        time.sleep(POLLING_INTERVAL)
 
 
-def get_all_nodes ():
+def get_all_nodes (states=None):
     p = scontrol('show nodes')
     for line in p.stdout:
-        for match in NODE_NAME_P.finditer(line):
-            yield match.group(1)
+        if states is not None:
+            if STATE_P.search(line).group(1).lower() not in [state.lower() for state in states]:
+                continue
+            else:
+                yield NODE_NAME_P.search(line).group(1)
     p.wait()
 
 
@@ -43,14 +77,27 @@ def get_partition_nodes (partition):
     p.wait()
 
 
-def srun (_exe='/usr/bin/srun'):
-    args = [_exe]
-    p = subprocess.Popen(args)
-    p.communicate()
+def srun (executable, executable_arguments, partition=None, nodelist=None, ntasks=None,
+          account=None, chdir=None, time=None, srun_='/usr/bin/srun'):
+    args = [srun_]
+    if partition:
+        args.extend(('--partition', partition))
+    if nodelist:
+        args.extend(('--nodelist', nodelist))
+    if ntasks:
+        args.extend(('--ntasks', ntasks))
+    if account:
+        args.extend(('--account', account))
+    if chdir:
+        args.extend(('--chdir', chdir))
+    if time:
+        args.extend(('--time', time))
+    args.append(executable)
+    return subprocess.Popen(args, stdout=subprocess.PIPE)
 
 
-def scontrol (command, _exe='/usr/bin/scontrol'):
-    args = [_exe]
+def scontrol (command, scontrol_='/usr/bin/scontrol'):
+    args = [scontrol_, '--oneliner']
     args.extend(command.split())
     return subprocess.Popen(args, stdout=subprocess.PIPE)
 
